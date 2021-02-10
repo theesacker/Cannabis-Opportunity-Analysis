f5316ba4c04392321948d462d7ba924214faf9b1
Leo <leonardorurrutia@gmail.com>
2021-02-10 11:22:50 -0800

    Added sales to geojson properties to color code county boundries

--- static/js/visualization2.js
+++ static/js/visualization2.js
@@ -3,7 +3,7 @@ var queryUrl = "static/data/oregon_counties.geojson";
 
 var myMap = L.map("mapid", {
   center: [44., -120.67],
-  zoom: 7
+  zoom: 8
 });
 
 var worldmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -39,8 +39,8 @@ d3.json(queryUrl).then(function (county) {
       return {
         color: "white",
         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: "blue",
-        fillOpacity: 0.5,
+        fillColor: countyColor(feature.properties.sales),
+        fillOpacity: 0.4,
         weight: 1.5
       };
     },
@@ -52,14 +52,14 @@ d3.json(queryUrl).then(function (county) {
         mouseover: function (event) {
           layer = event.target;
           layer.setStyle({
-            fillOpacity: 0.9
+            fillOpacity: 0.6
           });
         },
         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
         mouseout: function (event) {
           layer = event.target;
           layer.setStyle({
-            fillOpacity: 0.5
+            fillOpacity: 0.4
           });
         }
         // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
@@ -68,7 +68,7 @@ d3.json(queryUrl).then(function (county) {
         // }
       });
       // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h2>" + feature.properties.altname + " County</h2>");
+      layer.bindPopup("<h3>" + feature.properties.altname + " County</h3><h3> Sales in 2019: $" + feature.properties.sales + "</h3>");
     }
   }).addTo(countyArea);
 });
@@ -91,7 +91,6 @@ d3.json(queryUrlRetail).then(function (data) {
     jsonFeatures.push(feature);
   })
   var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
-  console.log(geoJson)
   L.geoJson(geoJson, {
     pointToLayer: function (feature, latlng) {
       return L.marker(latlng);
@@ -123,23 +122,51 @@ d3.json(queryUrlCounty).then(function (data) {
     jsonFeatures.push(feature);
   })
   var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
-  console.log(geoJson)
   L.geoJson(geoJson, {
     pointToLayer: function (feature, latlng) {
       return L.marker(latlng);
     },
     onEachFeature: function (feature, layer) {
       layer.bindPopup(`
-      ${feature.properties.county} County 
-        <br> Sales: $${feature.properties.sales} 
-        <br> Dispensary Count: ${feature.properties.dispensary_count}
-        <br> Avg. Sales: $${feature.properties.avg_sales_per_dispensary}
-        <br> Population: ${feature.properties.population}
-        <br> Avg. Income: $${feature.properties.per_capita_income}
-        <br> % of sales over Income: ${feature.properties.percent_of_sales_over_income}%
+      <strong>${feature.properties.county} County</strong> 
+        <br> Sales: <strong>$${feature.properties.sales}</strong> 
+        <br> Dispensary Count: <strong>${feature.properties.dispensary_count}</strong>
+        <br> Avg. Sales: <strong>$${feature.properties.avg_sales_per_dispensary}</strong>
+        <br> Population: <strong>${feature.properties.population}</strong>
+        <br> Avg. Income: <strong>$${feature.properties.per_capita_income}</strong>
+        <br> % of sales over Income: <strong>${feature.properties.percent_of_sales_over_income}%</strong>
         `)
     }
   }).addTo(countySales);
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+  // // code to try and make map legend
+  // var legend = L.control({ position: 'bottomleft' });
+
+  // legend.onAdd = function (map) {
+
+  //   var div = L.DomUtil.create('div', 'info legend'),
+  //     grades = [0, 500000, 1000000, 5000000, 10000000, 25000000, 50000000, 200000000],
+  //     labels = [];
+
+  //   // loop through our density intervals and generate a label with a colored square for each interval
+  //   for (var i = 0; i < grades.length; i++) {
+  //     div.innerHTML +=
+  //       '<i style="background:' + depthColor(grades[i] + 1) + '"></i> ' +
+  //       grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  //     console.log(grades[i])
+  //     console.log(grades[i + 1])
+  //   }
+
+  //   return div;
+  // };
+
+  // legend.addTo(myMap);
+
 });
 
 
@@ -147,18 +174,28 @@ d3.json(queryUrlCounty).then(function (data) {
 // Linking Maps with Functions/Data
 var baseMaps = {
   "World Map": worldmap,
-  "Gray Scale" : greyscaleMap
+  "Gray Scale": greyscaleMap
 };
 var overlayMaps = {
-  "Store": stores,
   "County Boundaries": countyArea,
-  "County Data" : countySales
+  "Dispensaries": stores,
+  "County Data": countySales
 };
 
 
-// add layer to control map
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+// // add layer to control map
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false
+// }).addTo(myMap);
 
 
+function countyColor(county) {
+  return county >= 200000000 ? '#63f542' :
+    county >= 50000000 ? '#42f599' :
+      county >= 25000000 ? '#42f5ec' :
+        county >= 10000000 ? '#42adf5' :
+          county >= 5000000 ? '#7534f7' :
+            county >= 1000000 ? '#fa20f6' :
+              county >= 500000 ? '#fa20a3' :
+                '#fa2020';
+};
